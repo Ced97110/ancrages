5e2c3f93d979e6d1c385ccebe12054b85d1aae71
ced <ced@ceds-MacBook-Air.local>
2023-08-14 19:15:48 -0700

    jk

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,6 +5,6 @@ export { default as Footer } from './footer';
 export { default as Loader } from './loader';
 export { default as Menu } from './menu';
 export { default as Hero } from './sections/hero';
-export { default as Featured } from './sections/Featured';
-export { default as Brand } from './sections/Brand';
+export { default as Featured } from './sections/featured';
+export { default as Brand } from './sections/brand';
 export { default as ServicesIntro } from './sections/serviceIntro';
